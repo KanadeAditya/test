d621dddc5c3839bde23bb084f24bfbcc1b5611b8
KanadeAditya <110182805+KanadeAditya@users.noreply.github.com>
2023-02-19 12:29:36 +0530

    some changes

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ require("dotenv").config();
 const {connection} = require("./db.js")
 const {userRouter} = require('./controller/users.routes.js')
 const {todosRouter}= require("./controller/todos.routes.js");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
-
+app.use(cors())
 app.use("/users",userRouter);
 app.use("/todos",todosRouter);
 
